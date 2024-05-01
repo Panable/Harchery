0a536815cd107e1817254f028f9b92521be0b026
SanyaBaweja <bawejasanya2602@gmail.com>
2024-05-01 22:34:42 +1000

    Changes for competition scores

--- harchery/app/views/archer/index.php
+++ harchery/app/views/archer/index.php
@@ -8,6 +8,8 @@
             <div class="card-body text-center">
                 <h1 class="card-text">Mr Archer. What do ye want do?</h1>
                 <a class="link-secondary" href=<?php echo URLROOT; ?>archer/viewScore>1. View archer's score</a>
+                <a class="link-secondary" href=<?php echo URLROOT; ?>archer/viewCompetitionResults>2. View competition result </a>
+                
             </div>
         </div>
     </main>

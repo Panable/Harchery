97114b5ea9c9e2d0d417339014a3120e55832e02
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-05-14 15:32:46 +1000

    Remove index for now

--- init.sql
+++ init.sql
@@ -49,7 +49,6 @@ CREATE TABLE Archer (
 	DOB DATE,
 	Gender VARCHAR(255),
 	ClubID INT,
-	INDEX ClubID,
 	FOREIGN KEY (ClubID) REFERENCES Club(ID)
 );
 
@@ -59,7 +58,6 @@ CREATE TABLE RoundRecord (
 	RoundID INT,
 	Equipment VARCHAR(255),
 	ArcherID INT,
-	INDEX ArcherID,
 	FOREIGN KEY (RoundID) REFERENCES `Round`(ID),
 	FOREIGN KEY (Equipment) REFERENCES Division(Equipment),
 	FOREIGN KEY (ArcherID) REFERENCES Archer(ID)
@@ -76,7 +74,6 @@ CREATE TABLE Arrow (
 	RoundRecordID INT,
 	PertainingEnd INT,
 	Score INT,
-	INDEX RoundRecordID,
 	FOREIGN KEY (RoundRecordID) REFERENCES RoundRecord(ID)
 );
 

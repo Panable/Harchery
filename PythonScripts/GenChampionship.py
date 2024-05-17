8800c4f5d7f5a9d53d434d72e1223329cfdb065b
a0re <102201308+a0re@users.noreply.github.com>
2024-05-17 22:53:06 +1000

    Added Condition check to avoid duplicate competitionIDs

--- PythonScripts/GenChampionship.py
+++ PythonScripts/GenChampionship.py
@@ -4,14 +4,23 @@ import random
 # Instantiate Faker
 fake = Faker()
 
+
 # Function to generate fake data for Championship table
 def generate_fake_championship_data(num_records):
     data = []
-    for _ in range(num_records):
+    used_competition_ids = set()  # Set is used to store unique competition IDs, to avoid duplicates
+    max_competition_id = 100  # Maximum competition ID
+    while len(used_competition_ids) < max_competition_id:
         club_id = random.randint(1, 150)
-        competition_id = random.randint(1, 100)
+        while True:
+            competition_id = random.randint(1, max_competition_id)
+            # Condition check to avoid duplicate competition IDs
+            if competition_id not in used_competition_ids:
+                used_competition_ids.add(competition_id)
+                break
         data.append((club_id, competition_id))
     return data
+
 
 # Insert fake data into a file
 def insert_fake_data_to_file(file_path, data):
@@ -26,8 +35,11 @@ def insert_fake_data_to_file(file_path, data):
         sql_file.write(";")
         sql_file.write("SET FOREIGN_KEY_CHECKS = 1;")
 
+
 # Generate fake championship data
 fake_championship_data = generate_fake_championship_data(150)
 
 # Modify Parameters: file_path & fake_championship_data
 insert_fake_data_to_file("fake_championship.sql", fake_championship_data)
+
+print("Fake Championship Data Generated Successfully!")

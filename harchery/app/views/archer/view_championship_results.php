8c7f67fc1eb3000ca723ef7b8bb983c2b33e9e12
SanyaBaweja <bawejasanya2602@gmail.com>
2024-05-17 22:13:52 +1000

    I am just tired of fixing this shit code

--- harchery/app/views/archer/view_championship_results.php
+++ harchery/app/views/archer/view_championship_results.php
@@ -30,60 +30,75 @@
             if (empty($club_id)) {
                 echo "<p>Please specify the club ID.</p>";
             } else {
-           
+                $query = "
+                    SELECT C.CompetitionID, Comp.Name AS CompetitionName
+                    FROM Championship AS C
+                    JOIN Competition AS Comp ON C.CompetitionID = Comp.ID
+                    WHERE C.ClubID = $club_id
+                ";
+                $championships_result = mysqli_query($conn, $query);
+                
+                if (mysqli_num_rows($championships_result) > 0) {
+                    $has_scores = false;
+                    $max_score = 0;
+                    $winner = "";
+                    echo "<h2>Club ID: $club_id</h2>";
+                    echo "<table>";
+                    echo "<thead><tr><th class='padded'>Competition Name</th><th class='padded'>Archer Name</th><th class='padded'>Score</th></tr></thead>";
+                    echo "<tbody>";
 
-            $query = "SELECT * FROM Championship WHERE ClubID = $club_id";
-            $championships_result = mysqli_query($conn, $query);
-      
+                    while ($championship = mysqli_fetch_assoc($championships_result)) {
+                        $competition_id = $championship['CompetitionID'];
+                        $competition_name = $championship['CompetitionName'];
 
-            while ($championship = mysqli_fetch_assoc($championships_result)) {
-                $championship_id = $championship['ClubID'];
-                $competition_id = $championship['CompetitionID'];
+                        $query = "SELECT 
+                                CONCAT(A.FirstName, ' ', A.LastName) AS ArcherFullName,
+                                SUM(Arr.Score) AS ArcherScore
+                            FROM 
+                                Archer AS A
+                                INNER JOIN RoundRecord AS RR ON A.ID = RR.ArcherID
+                                INNER JOIN Arrow AS Arr ON RR.ID = Arr.RoundRecordID
+                                INNER JOIN CompetitionRecord AS CR ON RR.ID = CR.RoundRecordID
+                            WHERE 
+                                CR.CompetitionID = $competition_id
+                            GROUP BY 
+                                A.ID
+                        ";
 
+                        $archers_result = mysqli_query($conn, $query);
 
-                $query = "SELECT 
-                CONCAT(A.FirstName, ' ', A.LastName) AS ArcherFullName,
-                SUM(Arr.Score) AS ArcherScore
-            FROM 
-                Archer AS A
-                INNER JOIN RoundRecord AS RR ON A.ID = RR.ArcherID
-                INNER JOIN Arrow AS Arr ON RR.ID = Arr.RoundRecordID
-                INNER JOIN CompetitionRecord AS CR ON RR.ID = CR.RoundRecordID
-            WHERE 
-                CR.CompetitionID = $competition_id
-            GROUP BY 
-                A.ID";
-            
-            
-                $archers_result = mysqli_query($conn, $query);
-                $max_score = 0;
-                $winner = "";
+                        if (mysqli_num_rows($archers_result) > 0) {
+                            $has_scores = true;
 
-                echo "<h2>Club ID: $championship_id</h2>";
-                echo "<table>";
-                echo "<thead><tr><th class='padded'>Archer Name</th><th class='padded'>Score</th></tr></thead>";
-                echo "<tbody>";
+                            while ($archer = mysqli_fetch_assoc($archers_result)) {
+                                if ($archer['ArcherScore'] > $max_score) {
+                                    $max_score = $archer['ArcherScore'];
+                                    $winner = $archer['ArcherFullName'];
+                                }
 
-                while ($archer = mysqli_fetch_assoc($archers_result)) {
-                    // Find the archer with the highest score
-                    if ($archer['ArcherScore'] > $max_score) {
-                        $max_score = $archer['ArcherScore'];
-                        $winner = $archer['ArcherFullName'];
+                                echo "<tr>";
+                                echo "<td>{$competition_name}</td>";
+                                echo "<td>{$archer['ArcherFullName']}</td>";
+                                echo "<td>{$archer['ArcherScore']}</td>";
+                                echo "</tr>";
+                            }
+                        }
                     }
 
-                    echo "<tr>";
-                    echo "<td>{$archer['ArcherFullName']}</td>";
-                    echo "<td>{$archer['ArcherScore']}</td>";
-                    echo "</tr>";
+                    if (!$has_scores) {
+                        echo "<tr><td colspan='3'>No competition records found for this club.</td></tr>";
+                    }
+
+                    echo "</tbody></table>";
+                    if ($has_scores) {
+                        echo "<br><hr>";
+                        echo "<p>Winner: $winner</p>";
+                    }
+                } else {
+                    echo "<p>No championships found for Club ID: $club_id.</p>";
                 }
-                echo "</tbody></table>";
-                echo "<br><hr>";
-                echo "<p>Winner: $winner</p>";
             }
-        
         }
-    }
-        
 
         mysqli_close($conn);
         ?>
